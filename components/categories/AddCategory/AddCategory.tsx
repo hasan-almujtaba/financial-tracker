e8e4187002c62bd43ede4b98b31acdbbbfc9efe2
Hasan Al Mujtaba <hasan.almujtaba01@Gmail.com>
2022-07-10 14:42:48 +0700

    add category add functionality

---
+++ components/categories/AddCategory/AddCategory.tsx
@@ -0,0 +1,97 @@
+import { Button, Modal, Select, TextInput } from '@mantine/core'
+import { useForm } from '@mantine/hooks'
+import { useState } from 'react'
+import useStore from 'store'
+import useStyles from './AddCategory.styles'
+
+const AddCategory = () => {
+  /**
+   * Use styles from useStyles variable
+   */
+  const { classes } = useStyles()
+
+  /**
+   * Zustand state for getting las id on store
+   */
+  const lastId = useStore((state) => state.getCategoryLastId)
+  const addCategory = useStore((state) => state.addCategory)
+
+  /**
+   * Open state for modal
+   */
+  const [opened, setOpened] = useState(false)
+
+  /**
+   * Select Option
+   */
+  const selectOptions = [
+    { value: 'expense', label: 'Expense' },
+    { value: 'income', label: 'Income' },
+  ]
+
+  /**
+   * Handle form input
+   */
+  const form = useForm({
+    initialValues: {
+      name: '',
+      type: '',
+    },
+  })
+
+  /**
+   * Store new category on form submit
+   * @param values - Form value
+   */
+  const onFormSubmit = (values: typeof form.values) => {
+    const categoryLastId = lastId()
+    const newCategory = {
+      id: categoryLastId + 1,
+      name: values.name,
+      type: values.type,
+    }
+
+    addCategory(newCategory)
+
+    setOpened(false)
+    form.reset()
+  }
+
+  return (
+    <div className={classes.container}>
+      <Button onClick={() => setOpened(true)}>Add Category</Button>
+
+      <Modal
+        opened={opened}
+        title="Add New Category"
+        centered
+        onClose={() => setOpened(false)}
+      >
+        <form onSubmit={form.onSubmit(onFormSubmit)}>
+          <TextInput
+            className={classes.input}
+            placeholder="Name"
+            label="Category Name"
+            {...form.getInputProps('name')}
+          />
+          <Select
+            placeholder="Type"
+            label="Category Type"
+            data={selectOptions}
+            className={classes.input}
+            {...form.getInputProps('type')}
+          />
+
+          <Button
+            fullWidth
+            type="submit"
+          >
+            Save
+          </Button>
+        </form>
+      </Modal>
+    </div>
+  )
+}
+
+export default AddCategory
