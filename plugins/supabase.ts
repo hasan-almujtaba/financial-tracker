8fa5c3a96b911a5d6a9c9c8709985feae9f8f988
Hasan Al Mujtaba <hasan.almujtaba01@Gmail.com>
2022-07-11 12:09:52 +0700

    cast env to string

--- plugins/supabase.ts
+++ plugins/supabase.ts
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js'
  * Supabase configuration
  * @see https://supabase.com/docs/reference/javascript/initializing
  */
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
