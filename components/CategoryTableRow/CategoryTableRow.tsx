fba6e7c8370f3519c4a846fffb978ee6c00ca4d3
Hasan Al Mujtaba <hasan.almujtaba01@Gmail.com>
2022-07-23 20:32:46 +0700

    add delete confirmation dialog

--- components/CategoryTableRow/CategoryTableRow.tsx
+++ components/CategoryTableRow/CategoryTableRow.tsx
@@ -1,5 +1,8 @@
 import { CategoryTableRowProps } from '@/types/category'
-import { Button } from '@mantine/core'
+import { Button, Text } from '@mantine/core'
+import { useModals } from '@mantine/modals'
+import { showNotification } from '@mantine/notifications'
+import { BsCheck } from 'react-icons/bs'
 import useStyles from './CategoryTableRow.styles'
 
 const CategoryTableRow = ({
@@ -14,10 +17,31 @@ const CategoryTableRow = ({
   const { classes } = useStyles()
 
   /**
+   * Hooks for using modal
+   */
+  const modals = useModals()
+
+  /**
    * Remove category from store
    */
   const onDeleteButtonClick = () => {
-    removeCategory(id)
+    modals.openConfirmModal({
+      title: 'Delete Category?',
+      labels: { confirm: 'Yes, Delete it', cancel: 'Cancel' },
+      children: (
+        <Text size="sm">This action will remove selected category</Text>
+      ),
+      onConfirm: () => {
+        removeCategory(id)
+        setTimeout(() => {
+          showNotification({
+            title: 'Success',
+            message: 'Category successfully deleted',
+            icon: <BsCheck size={18} />,
+          })
+        }, 500)
+      },
+    })
   }
 
   return (
